a546288eefc933d7966756db5995d93dd1d42ae4
Francois Nabwangu <fnabwangu@gmail.com>
2026-08-16 17:46:49 -0400

    Expose console component render functions in package init

--- console/components/__init__.py
+++ console/components/__init__.py
@@ -1,1 +1,14 @@
-# console components
+"""Console components package.
+
+This package exposes the individual Streamlit component render functions so callers
+(can import from console.components) have a single import point.
+"""
+from .signal_feed import render_signal_feed
+from .gate_indicators import render_gate_indicators
+from .approval_queue import render_approval_queue
+
+__all__ = [
+    "render_signal_feed",
+    "render_gate_indicators",
+    "render_approval_queue",
+]
